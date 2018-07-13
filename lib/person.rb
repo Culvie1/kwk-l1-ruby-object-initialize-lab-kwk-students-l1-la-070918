f475399c11ff451a160d9e08305a1f638e3f310e
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-13 21:27:23 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,3 +6,6 @@ puts "What kind of dog would you like?"
 dog = gets.strip
 puts "You would like a #{dog}."
 puts "You can get a #{dog} at a shelter or adoption center."
+puts "What would you like to name your #{dog}?"
+dog_name = gets.strip
+puts "Your dog's name will be #{dog_name}."
