aea847af7749efbc0560190081815a5cabaa2ca6
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-13 21:25:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+puts "What is your name?"
+name = gets.strip
+puts "Your name is #{name}."
+puts "What kind of dog would you like?"
+dog = gets.strip
+puts "You would like a #{dog}."
+puts "You can get a #{dog} at a shelter or adoption center."
